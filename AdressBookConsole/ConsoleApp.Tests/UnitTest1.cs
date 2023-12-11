8a65528e00cb8deb7fda9c09a6dda692be896ad3
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2023-12-11 19:00:12 +0100

    Add project files.

---
+++ AdressBookConsole/ConsoleApp.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBookConsole.ConsoleApp.Tests
+{
+    internal class UnitTest1
+    {
+    }
+}
